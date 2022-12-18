93ced1d94b2c9713ac87a24b736fd4777814be74
George <george.lungu1896@gmail.com>
2022-12-18 21:50:08 +0200

    list of movies or array of objects

---
+++ my-top-4-movies.js
@@ -0,0 +1,31 @@
+let favMovies = [
+    {
+        title: "The Wolf of Wall Street",
+        director: "Martin Scorsese",
+        yearOfRelease: 2013,
+        imdbRating: 8.2,
+        shortDescription: "Based on the true story of Jordan Belfort, from his rise to a wealthy stock-broker living the high life to his fall involving crime, corruption and the federal government."
+    },
+    {
+        title: "2001: A Space Odyssey",
+        director: "Stanley Kubrick",
+        yearOfRelease: 1968,
+        imdbRating: 8.3,
+        shortDescription: "After uncovering a mysterious artifact buried beneath the Lunar surface, a spacecraft is sent to Jupiter to find its origins - a spacecraft manned by two men and the supercomputer H.A.L. 9000."
+    },
+    {
+        title: "Pulp Fiction",
+        director: "Quentin Tarantino",
+        yearOfRelease: 1994,
+        imdbRating: 8.9,
+        shortDescription: "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption."
+    },
+    {
+        title: "Black Cat, White Cat",
+        director: "Emir Kusturica",
+        yearOfRelease: 1998,
+        imdbRating: 8.0,
+        shortDescription: "Matko and his son Zare live on the banks of the Danube river and get by through hustling and basically doing anything to make a living. In order to pay off a business debt Matko agrees to marry off Zare to the sister of a local gangster."
+        
+    }
+];
